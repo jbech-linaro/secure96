94ebf1654ece7bf42e30eed2c87d16a5c0cc99e7
Joakim Bech <joakim.bech@linaro.org>
2017-08-31 21:40:21 +0000

    Fix compiler error when enable extra debug
    Signed-off-by: Joakim Bech <joakim.bech@linaro.org>

--- include/debug.h
+++ include/debug.h
@@ -21,8 +21,10 @@
 
 #ifdef DEBUG
 #define logd(fmt, ...) \
-		fprintf(stdout, "[%s : %d]: " fmt,
-			__func__, __LINE__, ##__VA_ARGS__;
+		fprintf(stdout, "[%s : %d]: " fmt,  __func__, __LINE__, ##__VA_ARGS__);
+
+#define loge(fmt, ...) \
+		fprintf(stderr, "[%s : %d]: " fmt, __func__, __LINE__, ##__VA_ARGS__);
 #else
 #define logd(fmt, ...)
 #endif
